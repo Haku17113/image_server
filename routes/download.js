c513c97e0488a93123e6b331e2029f3a3e916d87
Haku17113 <riku0925h@gmail.com>
2018-09-24 23:34:51 +0900

    add: 画像のダウンロード機能
    URL/download/でfiles/sky1.jpgのダウンロード

--- routes/download.js
+++ routes/download.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  var extension = '.jpg';
+
+  var fileURL = './files/sky1' + extension;
+  var fileName = 'image' + extension;
+
+  res.download(fileURL, fileName, function(err){
+    if(err){
+      console.log('ERROR');
+    }else{
+      console.log('Download done');
+    }
+  });
 });
 
 module.exports = router;
